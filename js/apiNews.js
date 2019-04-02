8b8e0a2388187f698840923ef8c372f76ef68084
karo <karoxx98@gmail.com>
2019-04-02 17:55:36 +0200

    he creado un nuevo archivo apiNews.js y he convertido el slider en una funcion

---
+++ js/apiNews.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", () => {
+
+    var ajax = new XMLHttpRequest();
+    
+    ajax.open("GET", "https://newsapi.org/v2/top-headlines?sources=google-news-ar&apiKey=308ccf64283c4e479da642742b092324", true);
+    ajax.onreadystatechange = cargarNoticias;
+    ajax.send(null);
+    var imagenes = [];
+    
+    slider(imagenes)
+
+    function cargarNoticias(){
+
+        if(ajax.readyState == 4){
+
+             let datosNoticias = ajax.response;
+             let articulos = JSON.parse(datosNoticias);
+             
+            articulos.articles.forEach(e => {
+
+                let contenedorNoticia = document.querySelector(".cont-noticia");
+
+                // let infoNoticias = `
+
+                //     <img src="" class="img-noticia">
+                //     <h2 class="titulo-slider-noticia">${e.title}</h2>
+                //     <h3 class="subt-slider-noticia">${e.description}</h3>
+                // `;
+                // contenedorNoticia.insertAdjacentHTML("beforeend", infoNoticias);
+
+                imagenes.push(e.urlToImage)
+
+             });
+        }
+    }
+});
--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,37 @@
-window.addEventListener("load", () => {
-    
-    var botonAtras = document.querySelector(".atras-slider");
-    var botonDelante = document.querySelector(".delante-slider");
-    var imagenes = ["imagen1.jpg", "imagen2.jpg", "imagen3.jpg", "imagen4.jpg", "imagen5.jpg"];
-    var cont = 0;
-    var img = document.querySelector(".img-noticia");   
-    
-    botonAtras.addEventListener("click", retrocederImg);
-    botonDelante.addEventListener("click", pasarImg);
+
+   function slider(imgNot) {
+       
+       let botonAtras = document.querySelector(".atras-slider");
+       let botonDelante = document.querySelector(".delante-slider");
+       let cont = 0;
+       let imgNoticias = imgNot;
+       let img = document.querySelector(".img-noticia");   
+       
+       botonAtras.addEventListener("click", retrocederImg);
+       botonDelante.addEventListener("click", pasarImg);
+       
+   
+   
+       function pasarImg() {
+   
+           if (cont >= imgNoticias.length - 1) {
+               cont = 0;
+           }else{
+               cont++;
+           }
+           img.src = imgNoticias[cont];
+       }
+   
+       function retrocederImg() {
+   
+           if (cont <= 0) {
+               cont = imgNoticias.length - 1;
+           }else{
+               cont--;
+           }
+           img.src = imgNoticias[cont];
+       }
+   } 
 
 
 
-    function pasarImg() {
-
-        if (cont >= imagenes.length - 1) {
-            cont = 0;
-        }else{
-            cont++;
-        }
-        img.src = "./img/" + imagenes[cont];
-    }
-
-    function retrocederImg() {
-
-        if (cont <= 0) {
-            cont = imagenes.length - 1;
-        }else{
-            cont--;
-        }
-        img.src = "./img/" + imagenes[cont];
-    }
-
-
-
-});
