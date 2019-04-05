dbd6f4b15b956066222d422bc2a7599e65001943
unknown <devweb@aula.local>
2019-04-05 13:25:58 +0200

    he hecho que el slider sea automatico

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,53 @@
 
-   function slider(imgNot) {
-       
-       let botonAtras = document.querySelector(".atras-slider");
-       let botonDelante = document.querySelector(".delante-slider");
-       let cont = 0;
-       let imgNoticias = imgNot;
-       let img = document.querySelector(".img-noticia");   
-       
-       botonAtras.addEventListener("click", retrocederImg);
-       botonDelante.addEventListener("click", pasarImg);
-       
-   
-   
-       function pasarImg() {
-   
-           if (cont >= imgNoticias.length - 1) {
-               cont = 0;
-           }else{
-               cont++;
-           }
-           img.src = imgNoticias[cont];
-       }
-   
-       function retrocederImg() {
-   
-           if (cont <= 0) {
-               cont = imgNoticias.length - 1;
-           }else{
-               cont--;
-           }
-           img.src = imgNoticias[cont];
-       }
-   } 
+function slider(imgNot) {
+
+    let botonAtras = document.querySelector(".atras-slider");
+    let botonDelante = document.querySelector(".delante-slider");
+    let cont = 0;
+    let imgNoticias = imgNot;
+    let img = document.querySelector(".img-noticia");
+    let slide
+
+    botonAtras.addEventListener("click", retrocederImg);
+    botonDelante.addEventListener("click", pasarImg);
+
+
+
+    function pasarImg() {
+
+        if (cont >= imgNoticias.length - 1) {
+            cont = 0;
+        } else {
+            cont++;
+        }
+        img.src = imgNoticias[cont];
+
+        // stopSlide();
+    }
+
+    function retrocederImg() {
+
+        if (cont <= 0) {
+            cont = imgNoticias.length - 1;
+        } else {
+            cont--;
+        }
+        img.src = imgNoticias[cont];
+
+        // stopSlide();
+    }
+    
+    // function stopSlide() {
+    //     clearInterval(slide);
+    // }
+
+    function playSlide() {
+        ;
+        slide = setInterval(pasarImg, 5000);
+    }
+
+    // playSlide()
+}
 
 
 
